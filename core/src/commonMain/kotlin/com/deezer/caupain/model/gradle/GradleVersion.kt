690e70b4d6a614d535d34d404924a441c3545505
Valentin Rocher <vrocher@deezer.com>
2026-08-22 14:19:02 +0200

    chore: Minor fix for gradle version parsing

--- core/src/commonMain/kotlin/com/deezer/caupain/model/gradle/GradleVersion.kt
+++ core/src/commonMain/kotlin/com/deezer/caupain/model/gradle/GradleVersion.kt
@@ -21,6 +21,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
+ * This file incorporates work covered by the following copyright and permission notice:
+ *
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package com.deezer.caupain.model.gradle
@@ -38,6 +54,7 @@ public class GradleVersion(
     public val isSnapshot: Boolean = isGradleSnapshotVersion(version)
 ) {
     private companion object {
+        // Taken from GradleVersionResolver in Gradle sources
         val GRADLE_VERSION_PATTERN =
             Regex("((\\d+)(\\.\\d+)+)(-(\\p{Alpha}+)-(\\w+))?(-(SNAPSHOT|\\d{14}([-+]\\d{4})?))?")
 
